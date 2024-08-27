78827a7320f96b6c21e2272e8362f5352a50699e
Code Legion <piustxd@gmail.com>
2024-08-28 03:51:24 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator!")
+city = input("Please enter the name of the city you grew up in: ")
+fav_pet = input("Please enter the name of your favourite pet: ")
+
+print(f"The name of your band should be '{city} {fav_pet}'")
